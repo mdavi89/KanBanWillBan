d253c738c941c4946e087ba63bf076c403da0afb
mdavi89 <mdavi359@yahoo.com>
2025-01-14 14:55:40 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,7 +5,13 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = new Sequelize(process.env.DB_URL);
+const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
